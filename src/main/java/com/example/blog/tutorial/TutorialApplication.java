25135ec10b856f87531c9b41abea1b2e520b97b9
abhishek270104 <abhishek.2024cs1184@kiet.edu>
2025-04-02 21:09:44 +0530

    ModelMapper added in the project and consumed in the service for transforming entity class to payload DTO

--- src/main/java/com/example/blog/tutorial/TutorialApplication.java
+++ src/main/java/com/example/blog/tutorial/TutorialApplication.java
@@ -1,7 +1,9 @@
 package com.example.blog.tutorial;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TutorialApplication {
@@ -9,5 +11,8 @@ public class TutorialApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TutorialApplication.class, args);
 	}
-
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 }
--- src/main/java/com/example/blog/tutorial/services/impl/UserServiceImpl.java
+++ src/main/java/com/example/blog/tutorial/services/impl/UserServiceImpl.java
@@ -5,60 +5,64 @@ import com.example.blog.tutorial.exceptions.ResourceNotFoundException;
 import com.example.blog.tutorial.payloads.UserDTO;
 import com.example.blog.tutorial.repositories.UserRepository;
 import com.example.blog.tutorial.services.UserServices;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ReflectionUtils;
 
 import java.lang.reflect.Field;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserServices {
 
     private final UserRepository userRepository;
+    private final ModelMapper modelMapper;
 
     @Autowired
-    public UserServiceImpl(UserRepository userRepository) {
+    public UserServiceImpl(UserRepository userRepository, ModelMapper modelMapper) {
         this.userRepository = userRepository;
+        this.modelMapper = modelMapper;
     }
     @Override
     public UserDTO createUser(UserDTO userDTO) {
-        User user = this.dtoToUser(userDTO);
+        User user = dtoToUser(userDTO);
         User savedUser = userRepository.save(user);
-        return this.userToDTO(savedUser);
+        return userToDTO(savedUser);
     }
 
     @Override
     public UserDTO updateUser(UserDTO userDTO, UUID id) {
         User user = userRepository.findById(id).orElseThrow(() -> (new ResourceNotFoundException("user", "id", id)));
-        for (Field field : UserDTO.class.getDeclaredFields()) {
+        ReflectionUtils.doWithFields(UserDTO.class, field -> {
             field.setAccessible(true);
-            try {
-                Object value = field.get(userDTO);
-                if (value != null && !"id".equals(field.getName())) {
-                    Field userField = User.class.getDeclaredField(field.getName());
+            Object value = field.get(userDTO);
+
+            if (value != null && !"id".equals(field.getName())) {
+                Field userField = ReflectionUtils.findField(User.class, field.getName());
+                if (userField != null) {
                     userField.setAccessible(true);
                     userField.set(user, value);
                 }
-            } catch (NoSuchFieldException | IllegalAccessException e) {
-                e.printStackTrace();
             }
-        }
+        });
 
         User updatedUser = userRepository.save(user);
 
-        return this.userToDTO(updatedUser);
+        return userToDTO(updatedUser);
     }
     @Override
     public List<UserDTO> getAllUsers(){
         List<User> users = userRepository.findAll();
-        return users.stream().map(this::userToDTO).toList();
+        return users.stream().map(this::userToDTO).collect(Collectors.toList());
     }
 
     @Override
     public UserDTO getUserById(UUID id) {
-        User user = this.userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("user", "id", id));
-        return this.userToDTO(user);
+        User user = userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("user", "id", id));
+        return userToDTO(user);
     }
     @Override
     public void deleteUser(UUID id){
@@ -67,22 +71,10 @@ public class UserServiceImpl implements UserServices {
     }
 
     private User dtoToUser(UserDTO userDto) {
-        User user = new User();
-        user.setId(userDto.getId());
-        user.setName(userDto.getName());
-        user.setEmail(userDto.getEmail());
-        user.setAbout(userDto.getAbout());
-        user.setPassword(userDto.getPassword());
-        return user;
+        return modelMapper.map(userDto, User.class);
     }
 
     private UserDTO userToDTO(User user) {
-        UserDTO userDTO = new UserDTO();
-        userDTO.setId(user.getId());
-        userDTO.setName(user.getName());
-        userDTO.setEmail(user.getEmail());
-        userDTO.setAbout(user.getAbout());
-        userDTO.setPassword(user.getPassword());
-        return userDTO;
+        return modelMapper.map(user, UserDTO.class);
     }
 }
